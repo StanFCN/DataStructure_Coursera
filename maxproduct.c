abc57ee14da6e8864f0cedc935f4c4cf158ef05d
stan fang <12094581+stan-fang@user.noreply.gitee.com>
2024-04-14 00:37:42 +0800

    v1.0

--- maxproduct.c
+++ maxproduct.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 
-int MaxPairwiseProduct(int* p,int length);
+int MaxPairwiseProduct(int *p, int length);
 
 int main()
 {
     int n;
     scanf("%d", &n); // input the number of elements in the array
     int a[n];
-    
-    for (int i = 0; i < n;i++)
+
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
-    int result = MaxPairwiseProduct(a,n);
+    int result = MaxPairwiseProduct(a, n);
     printf("%d\n", result);
 
     return 0;
 }
 
-int MaxPairwiseProduct(int* p,int length)
+int MaxPairwiseProduct(int *p, int length)
 {
     int result = 0;
     for (int i = 0; i < length; i++)
     {
         for (int j = i + 1; j < length; j++)
         {
-            if(p[i] * p[j] > result)
+            if (p[i] * p[j] > result)
                 result = p[i] * p[j];
         }
-        
     }
     return result;
 }
