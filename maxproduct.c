31a3724cb466a668ad758876c512af50a5f2b97d
fang_zk <1f12z23k3@gmail.com>
2024-04-16 03:40:23 +0800

    correct

--- maxproduct.c
+++ maxproduct.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 long long MaxPairwiseProduct(int *p, int length);
+long long FastMaxPairwiseProduct(int *p, int length);
 
 int main()
 {
@@ -12,9 +13,31 @@ int main()
     {
         scanf("%d", &a[i]);
     }
-    long long result = MaxPairwiseProduct(a, n);
-    printf("%lld\n", result);
-    // 将%lld改为%I64d 这使得MingW不再报错： unknown conversion type character 'l' in format [-Wformat]
+    /*     while (1)
+        { */
+    /*         int n = rand() % 10 + 2;
+            int a[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rand() % 10;
+                printf("%d ", a[i]);
+
+            }
+            printf("\n"); */
+
+    long long result1 = FastMaxPairwiseProduct(a, n);
+    printf("%lld\n", result1);
+    /*         long long result2 = MaxPairwiseProduct(a, n);
+            if (result1 != result2)
+            {
+                printf("%lld  %lld\n", result1, result2);
+                break;
+            }
+            else
+                printf("ok\n");
+        } */
+
+    // 将%lld改为%I64d 这使得VScode不再报错： unknown conversion type character 'l' in format [-Wformat]
     return 0;
 }
 
@@ -32,5 +55,24 @@ long long MaxPairwiseProduct(int *p, int length)
     return result;
 }
 
-
-//FAST
+// FAST
+long long FastMaxPairwiseProduct(int *p, int length)
+{
+    int maxindex1 = -1;
+    int maxindex2 = -1;
+    for (int i = 0; i < length; i++)
+    {
+        if (maxindex1 == -1 || p[i] > p[maxindex1])
+        {
+            maxindex1 = i;
+        }
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (i != maxindex1 && (maxindex2 == -1 || p[i] > p[maxindex2]))
+        {
+            maxindex2 = i;
+        }
+    }
+    return (long long)p[maxindex1] * p[maxindex2];
+}
